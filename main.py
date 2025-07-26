0b1bd2d74be83828aec48eb9409409c5e05789d6
MVK2222 <sunnyjaya2203@gmail.com>
2025-07-26 20:59:03 +0530

    updated repository URL and added tech stack detection after ZIP file processing

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
     print("🤖 AI Code Review System ")
 
     # ---  1: Processing a Git Repository ---
-    repo_url = "https://github.com/Netaji2742/ChatBot"
+    repo_url = "https://github.com/sandeepdara-sd/ATS_Resume_Builder"
     print(f"\nProcessing Git URL: {repo_url}")
     git_temp_path = clone_repo(repo_url)
     # Initialize and run the detector
@@ -66,10 +66,22 @@ def main():
         zip_temp_path = extract_zip(selected_zip_path)
         if zip_temp_path:
             print(f"✅ ZIP file processed successfully. Code is in: {zip_temp_path}")
+            detector = TechStackDetector(zip_temp_path)
+            print("Running tech stack detection...")
+            # Run the detection
+            result_json = detector.run_detection()
+            print("Detecting technologies used in the codebase...")
+            if result_json:
+                print("Tech stack detection complete. Results:")
+                print(result_json)
+            else:
+                print("❌ Tech stack detection failed.")
+
         else:
             print(" Failed to process ZIP file.")
         # Run analysis on the extracted files
         run_analysis(zip_temp_path)
+
     else:
         print(" ZIP file processing skipped by user.")
 
